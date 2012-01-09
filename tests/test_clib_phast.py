958ae5c02eb59a45564615ead7bed29d37b31309
cev <ce@vejnar.org>
2012-01-09 17:43:17 +0100

    Changing libs path in tests

--- tests/test_clib_phast.py
+++ tests/test_clib_phast.py
@@ -7,7 +7,7 @@ import mirmap.if_clib_phast
 
 class TestPhast(unittest.TestCase):
     def test_get_exact_prob(self):
-        lib = mirmap.if_clib_phast.Phast(library_path='libs/compiled')
+        lib = mirmap.if_clib_phast.Phast(library_path='libs/default')
         aln = open('tests/input/NM_024573_ts1.fa').read()
         result = lib.phylop(method='SPH', mode='CONACC', mod_fname='tests/input/NM_024573.mod', aln=aln, aln_format='FASTA', prune=True)
         self.assertAlmostEqual(0.590965154, result)
--- tests/test_clib_rna.py
+++ tests/test_clib_rna.py
@@ -8,7 +8,7 @@ import mirmap.if_clib_rna
 class TestRNAvienna(unittest.TestCase):
     def test_fold(self):
         seq = 'AUCGAUGCGAUCGAGGGGCGCCCUUAAAGCUCUGAGGCGGCCCCCCA'
-        lib = mirmap.if_clib_rna.RNAvienna(library_path='libs/compiled')
+        lib = mirmap.if_clib_rna.RNAvienna(library_path='libs/default')
         lib.init_pf_fold(len(seq))
         struc_buffer = lib.get_string_buffer(len(seq))
         result = lib.pf_fold(seq, struc_buffer)
